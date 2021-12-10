955165a521d62a6f002eca064e89308db81d9bd3
Abbas <y.abbas.habib@gmail.com>
2021-12-10 20:56:32 +0200

    testing text change

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcom to Smart Silicon Egypt
         </a>
       </header>
     </div>
