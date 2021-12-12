78af4540082d4d18118b274da690c607e8914d1b
Abbas <y.abbas.habib@gmail.com>
2021-12-12 04:10:01 +0200

    Changed React Start interface

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Logo from './Logo'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Welcom to Smart Silicon Egypt
-        </a>
-      </header>
+      <Logo text1="⬇construction=(🏗)=>" text2="{SmartSiliconEgypt}"/>
     </div>
   );
 }
---
+++ src/Logo.js
@@ -0,0 +1,22 @@
+import './Logo.css';
+
+
+
+const Logo = ({ text1, text2 }) => {
+    return (
+        <div className="logo-containter">
+            <div className="outer-circle">
+                <div className="inner-circle">
+                    <p>{text1}</p>
+                    <p style={{color:'rgba(255,255,180,1)'}}>{text2}</p>
+                </div>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    );
+}
+
+export default Logo;
